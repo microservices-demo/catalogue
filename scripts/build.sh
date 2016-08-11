06e6200e8624a718107495d9aa7ab90207fb1dfc
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-08-11 17:48:44 +0200

    add build and push scripts

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -ev
+
+SCRIPT_DIR=$(dirname "$0")
+
+if [[ -z "$GROUP" ]] ; then
+    echo "Cannot find GROUP env var"
+    exit 1
+fi
+
+if [[ -z "$COMMIT" ]] ; then
+    echo "Cannot find COMMIT env var"
+    exit 1
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    DOCKER_CMD=docker
+else
+    DOCKER_CMD="sudo docker"
+fi
+CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
+echo $CODE_DIR
+
+REPO=${GROUP}/$(basename catalogue);
+$DOCKER_CMD build -t ${REPO}-dev $CODE_DIR/catalogue;
+$DOCKER_CMD create --name catalogue ${REPO}-dev;
+$DOCKER_CMD cp catalogue:/app/main $CODE_DIR/catalogue;
+$DOCKER_CMD rm catalogue;
+$DOCKER_CMD build -t ${REPO}:${COMMIT} -f $CODE_DIR/catalogue/Dockerfile-release $CODE_DIR/catalogue;
