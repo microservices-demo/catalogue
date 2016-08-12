e4f32a58d3412357467b5a50f0999429374a2aea
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-08-12 16:24:46 +0200

    remove api spec and add container test

--- test/container.py
+++ test/container.py
@@ -1,53 +1,47 @@
 import argparse
+import sys
 import unittest
 
-import sys
-from time import sleep
+import requests
 
-from util.Api import Api
 from util.Docker import Docker
-from util.Dredd import Dredd
 
 
 class CatalogueContainerTest(unittest.TestCase):
     TAG = "latest"
-    container_name = Docker().random_container_name('catalogue')
-    mysql_container_name = Docker().random_container_name('catalogue-db')
+    container_name = 'catalogue'
 
     def __init__(self, methodName='runTest'):
         super(CatalogueContainerTest, self).__init__(methodName)
         self.ip = ""
 
     def setUp(self):
-        db_command = ['docker', 'run', '-d', '-h', 'catalogue-db', '--name', self.mysql_container_name, '-e', 'MYSQL_ALLOW_EMPTY_PASSWORD=true', 'mysql']
-        Docker().execute(db_command)
         command = ['docker', 'run',
                    '-d',
                    '--name', CatalogueContainerTest.container_name,
-                   '-h', 'catalogue',
-                   '--link',
-                   self.mysql_container_name,
+                   '-h', CatalogueContainerTest.container_name,
                    'weaveworksdemos/catalogue:' + self.TAG]
         Docker().execute(command)
         self.ip = Docker().get_container_ip(CatalogueContainerTest.container_name)
 
     def tearDown(self):
         Docker().kill_and_remove(CatalogueContainerTest.container_name)
-        Docker().kill_and_remove(CatalogueContainerTest.mysql_container_name)
 
-    def test_api_validated(self):
-        limit = 60
-        while Api.noResponse('http://' + self.ip + ':80/catalogue'):
-            if limit == 0:
-                self.fail("Couldn't get the API running")
-            limit = limit - 1
-            sleep(1)
+    def test_catalogue_has_item_id(self):
+        r = requests.get('http://' + self.ip + '/catalogue', timeout=5)
+        data = r.json()
+        self.assertIsNotNone(data[0]['id'])
 
-        out = Dredd().test_against_endpoint("catalogue/catalogue.json", CatalogueContainerTest.container_name,
-                                            "http://catalogue/", self.mysql_container_name)
-        self.assertGreater(out.find("0 failing"), -1)
-        self.assertGreater(out.find("0 errors"), -1)
-        print(out)
+    def test_catalogue_has_image(self):
+        r = requests.get('http://' + self.ip + '/catalogue', timeout=5)
+        data = r.json()
+        for item in data:
+            for imageUrl in item['imageUrl']:
+                r = requests.get('http://' + self.ip + '/' + imageUrl, timeout=5)
+                self.assertGreater(int(r.headers.get("Content-Length")), 0,
+                                   msg="Issue with: " + imageUrl + ": " + r.headers.get("Content-Length"))
+                self.assertEqual("image/jpeg", r.headers.get("Content-Type"),
+                                 msg="Issue with: " + imageUrl + ": " + r.headers.get("Content-Type"))
 
 
 if __name__ == '__main__':
